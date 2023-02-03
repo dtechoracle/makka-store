8daa10085dcbe6a8771b28af97d94d540b4e3a78
dtechoracle <69088988+dtechoracle@users.noreply.github.com>
2023-02-03 12:44:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/layout.js";
 import Login from "./components/users/login.js";
 import All from "./components/allprducts/allproduct.js";
-import Productss from "./components/Products/products.js";
+// import Productss from "./components/Products/products.js";
 import View from "./components/Products/view.js";
 
 function App() {
@@ -13,7 +13,6 @@ function App() {
           <Route path="Login" element={<Login />} />
           <Route path="Products" element={<All />} />
           <Route path="View" element={<View />} />
-          <Route path="pp" element ={<Productss/>} />
       </Routes>
     </div>
   );
